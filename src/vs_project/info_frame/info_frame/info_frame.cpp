1869851531fdaf8de63506bdc8ffc8a07b45cc66
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2016-10-07 01:35:02 +0200

    Update info_frame.cpp

--- src/vs_project/info_frame/info_frame/info_frame.cpp
+++ src/vs_project/info_frame/info_frame/info_frame.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-	char sz[] = "Hello, Lotte!";	//Hover mouse over "sz" while debugging to see its contents
+	char sz[] = "COMPILED WITH VERSION AUTO COMPILER!";	//Hover mouse over "sz" while debugging to see its contents
 	cout << sz << endl;	//<================= Put a breakpoint here
 	return 0;
 }
