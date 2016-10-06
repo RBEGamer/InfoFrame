63608a2a5ebeec33681a2fa8af2a0fc6b6a34312
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2016-10-06 15:04:33 +0200

    added test project for version_checker_py

---
+++ src/vs_project/info_frame/info_frame/Debug/CMakeFiles/3.7.0-rc1/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Windows-10.0.14393")
+set(CMAKE_HOST_SYSTEM_NAME "Windows")
+set(CMAKE_HOST_SYSTEM_VERSION "10.0.14393")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "AMD64")
+
+include("C:/Users/marcel/Desktop/InfoFrame/src/vs_project/info_frame/info_frame/toolchain-Debug.cmake")
+
+set(CMAKE_SYSTEM "Linux")
+set(CMAKE_SYSTEM_NAME "Linux")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
---
+++ src/vs_project/info_frame/info_frame/Debug/CMakeFiles/info_frame.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/info_frame.dir/info_frame.cpp.o"
+  "info_frame.pdb"
+  "info_frame"
+  "info_frame.manifest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/info_frame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
